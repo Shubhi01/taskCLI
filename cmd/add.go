27126b3c8549118caf68ca3264c3893c9daa2341
Shubhi Rastogi <shubhi.rastogi@nutanix.com>
2020-01-15 16:26:03 +0530

    AddTask

--- cmd/add.go
+++ cmd/add.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"strings"
+	"task/db"
 
 	"github.com/spf13/cobra"
 )
@@ -11,8 +12,14 @@ var AddCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Adds a task",
 	Run: func(cmd *cobra.Command, args []string) {
-		task := strings.Join(args, " ")
-		fmt.Printf("Added task \"%s\"", task)
+		taskDescr := strings.Join(args, " ")
+		task := db.Task{Description: taskDescr}
+		err := db.AddTask(&task)
+		if err != nil {
+			fmt.Println("Failed to add task")
+		}
+		fmt.Printf("Added task \"%v\"", task)
+		// db.AddTask(task)
 	},
 }
 
